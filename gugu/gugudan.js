f6aa078bdf055f7722a873848156208afb6445c4
simhyorim <163081982+simhyorim@users.noreply.github.com>
2024-03-17 20:22:34 +0900

    주석 처리

--- gugu/gugudan.js
+++ gugu/gugudan.js
@@ -4,6 +4,7 @@ function printMultipltable(num) {
     for(let i=1; i <=9; i++){
         console.log(`${num} * ${i}=${num * i}`);
     }
+    //===========
 }
 for(let i=2; i<=9; i++){
     console.log(`${i} 단`);
